c1dd404e33f614152632743505cb13706bfa2a26
David Rimon Halim <96887854+davidrimon2004@users.noreply.github.com>
2025-05-16 17:07:25 +0300

    The logic of most of the SJFNP

--- OsProject/Form3.cs
+++ OsProject/Form3.cs
@@ -22,7 +22,11 @@ namespace OsProject
             public int WT;
         }
         List<prcss> process = new List<prcss>();
-        int flagc;
+         List<int> done = new List<int>();
+         int Time = 999;
+         int tot;
+         int min = 999;
+         int flagc;
         public Form3(int flag)
         {
             flagc = flag;
@@ -93,7 +97,41 @@ namespace OsProject
             textBox2.Clear();
 
         }
+        void calcSJFNP()
+        {
+            int imin = -1;
+            for (int i = 0; i < process.Count; i++)
+            {
+                if (done.Contains(i))
+                    continue;
+                if (process[i].burst < min && process[i].arrival <= Time)
+                {
+                    min = process[i].burst;
+                    imin = i;
+                    Time = process[i].arrival;
+                }
+            }
+            tot += process[imin].burst;
+            process[imin].CT = tot;
+            process[imin].TAT = process[imin].CT - process[imin].arrival;
+            process[imin].WT = process[imin].TAT - process[imin].burst;
 
+            if (process[imin].TAT < 0)
+            {
+                process[imin].TAT = 0;
+
+            }
+
+            if (process[imin].WT <= 0)
+            {
+                process[imin].WT = 0;
+
+            }
+
+            done.Add(imin);
+            Time = 999;
+            min = 999;
+        }
         private void button2_Click(object sender, EventArgs e)
         {
             if (flagc == 1)
@@ -106,7 +144,16 @@ namespace OsProject
             }
             else if (flagc == 3)
             {
-                
+                 for (int i = 0; i < process.Count; i++)
+                 {
+                     calcSJFNP();
+                 }
+                 for (int i = 0; i < process.Count; i++)
+                 {
+                     dataGridView1.Rows[i].Cells["wait"].Value = process[i].WT;
+                     dataGridView1.Rows[i].Cells["completion"].Value = process[i].CT;
+                     dataGridView1.Rows[i].Cells["TAT"].Value = process[i].TAT;
+                 }
             }
             else if (flagc == 4)
             {
