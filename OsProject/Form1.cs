ea933c76d2ecfc447f7f9b1a24ce4cdf8701411d
Omar El banna <bannxa2005@gmail.com>
2025-05-16 14:26:12 +0300

    Add files via upload

---
+++ OsProject/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OsProject
+{
+    public partial class Form1: Form
+    {
+        class prcss
+        {
+            public int ID;
+            public int arrival;
+            public int burst;
+            public int CT;
+            public int TAT;
+            public int WT;
+        }
+        List<prcss> process = new List<prcss>();
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            this.Load += Form1_Load;
+            MessageBox.Show("hello");
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("pid", "Process ID");
+            dataGridView1.Columns.Add("arrival", "Arrival Time");
+            dataGridView1.Columns.Add("burst", "Burst Time");
+            dataGridView1.Columns.Add("completion", "C.T.");
+            dataGridView1.Columns.Add("wait", "Waiting Time");
+            dataGridView1.Columns.Add("TAT", "T.A. Time");
+            dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            for (int i = 0; i < process.Count; i++)
+            {
+                dataGridView1.Rows.Add(process[i].ID, process[i].arrival, process[i].burst, process[i].CT, process[i].WT, process[i].TAT);
+            }
+        }
+
+    }
+}
